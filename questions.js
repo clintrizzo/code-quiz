039b6a2637589214ebbe90c30e48dd740967cccd
Clint <maxeva02@gmail.com>
2021-01-05 14:01:48 -0800

    wrote first question

---
+++ questions.js
@@ -0,0 +1,8 @@
+var questions = [{
+    "question 1": "Where is the correct place to insert a script tag?",
+    "option 1": "Inside the head tag.",
+    "option 2": "inside the title tag",
+    "option 3": "At the bottom of the body tag",
+    "option 4": "none of the above",
+    "answer": "option 3",
+}];
